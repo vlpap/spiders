886dc8f44f0712187ce99d2c025ef537ef688405
vlpap <papoyan8@gmail.com>
2018-02-26 22:24:09 +0300

    using list of pages

--- bank/spiders/theInsolvencyService.py
+++ bank/spiders/theInsolvencyService.py
@@ -1,25 +1,24 @@
 from pyspider.libs.base_handler import *
-from pprint import pprint
-import itertools
 from collections import OrderedDict
-import re
 import scrapy
-from bank.items import TheInsolvencyServiceItems
 
 class CaseDetails(scrapy.Spider):
 	name = "caseDetails"
-	start_urls = ['https://www.insolvencydirect.bis.gov.uk/piudb/viewpiucasedetails.asp?companyname=Foxton%20Furniture%20Group%20%20Ltd',]
+	start_urls = ['https://www.insolvencydirect.bis.gov.uk/piudb/viewqryl.asp']
 
 	def parse(self, response):
+		for href in response.xpath('//a [contains(@href, "companyname")]/@href'):
+			yield response.follow(href, self.parse_company)
+
+	def parse_company(self, response):
 		res = OrderedDict()
 		for x in response.xpath('//tr[2]//tr [position() > 1]'):
 			res[x.xpath('string(.//td [position() = 1])').extract()[0]] = x.xpath('string(.//td [position() = 2])').extract()[0]
 			
 		yield res
 		
-			
-
 	
+
 
 
 
