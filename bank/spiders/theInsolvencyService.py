2eb8ffca899c3d4c7190c7abad930b265bf881df
vlpap <papoyan8@gmail.com>
2018-02-26 15:11:43 +0300

    make some pretty and adduced to version for single page

--- bank/spiders/theInsolvencyService.py
+++ bank/spiders/theInsolvencyService.py
@@ -8,22 +8,14 @@ from bank.items import TheInsolvencyServiceItems
 
 class CaseDetails(scrapy.Spider):
 	name = "caseDetails"
-	start_urls = ['https://www.insolvencydirect.bis.gov.uk/piudb/viewpiucasedetails.asp?companyname-Foxton%20Furniture%20Group%20%20Ltd',]
+	start_urls = ['https://www.insolvencydirect.bis.gov.uk/piudb/viewpiucasedetails.asp?companyname=Foxton%20Furniture%20Group%20%20Ltd',]
 
 	def parse(self, response):
-	
-		trP = response.xpath('//tr [position() = 2]')
-		tr = trP.xpath('.//tr [position() > 1]')
 		res = OrderedDict()
-		i = 1
-		for x in tr:
+		for x in response.xpath('//tr[2]//tr [position() > 1]'):
 			res[x.xpath('string(.//td [position() = 1])').extract()[0]] = x.xpath('string(.//td [position() = 2])').extract()[0]
-
-			if i % 23 == 0:
-				yield res
-		
-			i += 1
-
+			
+		yield res
 		
 			
 
