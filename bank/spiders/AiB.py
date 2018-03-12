5e57281b0f616ff42f85f4c8b823f91443ab0705
vlpap <papoyan8@gmail.com>
2018-03-12 20:11:18 +0300

    testing spider AiB for few pages

--- bank/spiders/AiB.py
+++ bank/spiders/AiB.py
@@ -1,42 +1,36 @@
-from pyspider.libs.base_handler import *
-from collections import OrderedDict
-from collections import defaultdict
 import scrapy
+from collections import defaultdict
 
 class Liquidations(scrapy.Spider):
 	name = "liquidations"
 	start_urls = ['https://roi.aib.gov.uk/roi/Receiverships/Receivership/Details/1']
 	base_url = 'https://roi.aib.gov.uk/roi/Receiverships/Receivership/Details/'
 	
-	def crawlingOfLinks(self, response):
-		for link in range(1,3):
-			absolute_url = self.base_url + str(link)
-			yield scrapy.Request(absolute_url, callback=self.parse)
-		
-
 	def parse(self, response):
-		res = defaultdict(dict)
-		table = OrderedDict()
+		for link in range(1,291):
+			absolute_url = self.base_url + str(link)
+			yield scrapy.Request(absolute_url, callback=self.parse_company)
+	
+	def parse_company(self, response):
+		res = defaultdict(list)
+		table = dict()
 		tableHeader = list()
+		tableHeader = response.xpath('//h3/text()').extract()	
 		for x in response.xpath('//div [@class = "form-group"]'):
 			table[x.xpath('string( .//* [position() = 1])').extract_first()] = x.xpath('string( .//* [position() = 2])').extract_first()
-		
-		tableHeader = response.xpath('//h3/text()').extract()
-		res[tableHeader.pop()].update(table)
+			
+		res[tableHeader.pop()].append(table)
 				
 		for fieldset in response.xpath('//fieldset [position() > 1]'):
-			tableHeader += fieldset.xpath('.//legend/text()').extract()
 			th = fieldset.xpath('.//div//th/text()').extract()
-			td = list()
-			for x in  fieldset.xpath('.//div//tbody//td'):
-				td += x.xpath('string()').extract()
-			table = OrderedDict()
-			for x in range(0, len(td)):
-				table[th[x]] = td[x]
-			res[tableHeader.pop()].update(table)
+			for tr in fieldset.xpath('.//div//tbody//tr'):
+				tableHeader = fieldset.xpath('.//legend/text()').extract()			
+				td = list()
+				for x in tr.xpath('.//td'):
+					td += x.xpath('string()').extract()
+				table = dict()
+				for x in range(0, len(th)):
+					table[th[x]] = td[x]
+				res[tableHeader.pop()].append(table)
 			
 		yield res
-
-
-
-
