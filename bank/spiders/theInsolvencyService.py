e235abb7a0ca3333fb23824d6c1f69333d80504c
root <root@localhost.localdomain>
2018-04-17 14:24:06 +0000

    modified theIS for Kafka&NIFI

--- bank/spiders/theInsolvencyService.py
+++ bank/spiders/theInsolvencyService.py
@@ -2,8 +2,9 @@ from collections import OrderedDict
 from kafka import KafkaProducer
 import scrapy
 import json
+
 class CaseDetails(scrapy.Spider):
-	producer = KafkaProducer(bootstrap_servers = '10.93.221.39:9092')
+	producer = KafkaProducer(bootstrap_servers = '10.10.14.95:9092')
 	name = "caseDetails"
 	start_urls = ['https://www.insolvencydirect.bis.gov.uk/piudb/viewqryl.asp']
 
@@ -16,7 +17,7 @@ class CaseDetails(scrapy.Spider):
 		for x in response.xpath('//tr[2]//tr [position() > 1]'):
 			res[x.xpath('string(.//td [position() = 1])').extract()[0]] = x.xpath('string(.//td [position() = 2])').extract()[0]
 		
-		self.producer.send('theIS', json.dumps(res).encode('utf-8'))				
+		self.producer.send('test2', (json.dumps(res)).encode('utf-8'))				
 		yield res
 		
 	
